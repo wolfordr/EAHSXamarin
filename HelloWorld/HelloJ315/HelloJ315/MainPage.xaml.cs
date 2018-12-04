799c3578f2bd742a0899327c7c94add3972730cc
Michael Tanczos <mike@logic-gate.com>
2018-12-04 11:30:51 -0500

    Added Hello World Project

---
+++ HelloWorld/HelloJ315/HelloJ315/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloJ315
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            MyLabel.Text = "Button Pressed " + count + " Times!";
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            count++;
+            MyLabel.Text = "Cookie Pressed " + count + " Times!";
+        }
+    }
+}
